d01185aaf4d0a9ff78425ef5ae37a527a975b3a2
Khalil Al-rahman Youssefi Darmian <k1youssefi@edu.aau.at>
2022-08-09 01:03:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+    name='qrcodeT',
+    version=__version__,
+
+    url='https://github.com/Khalil-Youssefi/qrcodeT',
+    author='Khalil Youssefi',
+    author_email='kh4lil@outlook.com',
+
+    py_modules=['qrcodeT'],
+)
