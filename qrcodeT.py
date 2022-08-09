2b02855e881afde19164e9b8e4730629d10c1f54
k1youssefi@edu.aau.at <SlayerTheGreat10003>
2022-08-09 02:11:51 +0200

    bug fixes

--- qrcodeT.py
+++ qrcodeT.py
@@ -1,4 +1,4 @@
-__version__ = 'v1.0.0'
+__version__ = 'v1.0.2'
 import qrcode
 import numpy as np
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/Khalil-Youssefi/qrcodeT',
     author='Khalil Youssefi',
     author_email='kh4lil@outlook.com',
-
+    python_requires='>=3.6',
     py_modules=['qrcodeT'],
     install_requires=[
     'qrcode[pil]',
